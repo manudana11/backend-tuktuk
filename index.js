03cf9d22d8605123541b72e370ffd981ce7df045
manudana11 <manueldanapallardo@gmail.com>
2024-06-04 23:42:45 +0200

    probando

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const PORT = process.env.PORT || 3000;
 
 dbConnection()
 app.use(express.json())
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173/'
+}));
 app.use('/users', require('./routes/users'));
 app.use("/posts", require("./routes/posts"));
 app.use("/comments", require("./routes/comments"));
